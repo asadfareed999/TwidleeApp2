f25e20c96dc6fb3dc7af1dc980dd56ed727a8742
minhaj_irv <minhaj.nadeem@irvinetek.com>
2020-04-17 12:10:20 +0500

    updated retrofitinstance.kt
    1.Authorization header is not included when there is no token available.
    2.updated gitignore

--- app/src/main/java/com/example/asadfareed/twidlee2/utils/retrofitInstance.kt
+++ app/src/main/java/com/example/asadfareed/twidlee2/utils/retrofitInstance.kt
@@ -23,7 +23,17 @@ class retrofitInstance {
             sharedPref = context.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
             val token: String = sharedPref.getString("token_key", "")!!
             val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
-            if (token.isEmpty()) {
+            httpClient.addInterceptor { chain ->
+                val request = chain.request()
+                    .newBuilder()
+                    .addHeader("x-api-key", "5f7af37cb35f5cd8")
+                if (!token.isEmpty()){
+                    request.addHeader("Authorization", "Bearer " + token)
+                }
+
+                chain.proceed(request.build())
+            }
+            /*if (token.isEmpty()) {
                 httpClient.addInterceptor { chain ->
                     val request = chain.request()
                         .newBuilder()
@@ -42,7 +52,7 @@ class retrofitInstance {
 
                     chain.proceed(request)
                 }
-            }
+            }*/
             // added logging interceptor
             val httpLoggingInterceptor = HttpLoggingInterceptor()
             httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
